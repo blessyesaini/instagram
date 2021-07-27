d35abdd38106fd504704d052bd014314f489e94a
blessyesaini <blessy.elizabeth.saini@gmail.com>
2021-07-27 06:22:30 +0400

    initial init

---
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Blessy Elizabeth Saini on 7/26/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instagram/Controllers/Others/SearchResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResultViewController.swift
+//  Instagram
+//
+//  Created by Blessy Elizabeth Saini on 7/26/21.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  
+
+}
